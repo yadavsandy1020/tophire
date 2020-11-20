6a8145a3dea38e0679bd906ac4a20625b5be8c02
Sandeep Yadav <yadavsandy.1020@gmail.com>
2020-11-21 05:14:12 +0530

    added login screen

--- login-ui/src/App.svelte
+++ login-ui/src/App.svelte
@@ -1,30 +1,59 @@
 <script>
-	export let name;
 </script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
 
 <style>
 	main {
+		width: 60%;
+		height: 55%;
+		border: 1px solid rgb(223, 222, 222);
+		border-radius: 3px;
+	}
+	.flex-col {
+		display: flex;
+		flex-direction: column;
+		padding: 20px;
+	}
+	.center {
+		display: inline;
 		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
 	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+	.login {
+		background: blue;
+		color: white;
+		font-weight: bold;
+		border: 0px solid;
+		border-radius: 6px;
 	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	.new-account {
+		background: green;
+		color: white;
+		font-weight: bold;
+		border: 0px solid;
+		border-radius: 6px;
+		width: 44%;
+		margin-left: 25%;
+	}
+	input {
+		border: 0.3px solid black;
+		border-radius: 6px;
+	}
+	input::after {
+		border: 1px solid red;
 	}
 </style>
+
+<main>
+	<div class="flex-col">
+		<input type="text" placeholder="Email address or phone number" />
+		<input type="password" placeholder="Password" />
+		<button class="login">Login</button>
+	</div>
+
+	<div class="flex-col">
+		<span class="center"><a href="/">Forgotten password?</a></span>
+	</div>
+	<hr />
+	<div class="flex-col">
+		<button class="new-account">Create New Account</button>
+	</div>
+</main>
